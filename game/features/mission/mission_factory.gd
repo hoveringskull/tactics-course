c500dcfe21dc8ab48beca1bf7dce6c5acdc2ad40
Joshua Grant <jc.grant22@gmail.com>
2026-09-19 20:44:52 -0400

    Fix warns

--- game/features/mission/mission_factory.gd
+++ game/features/mission/mission_factory.gd
@@ -1,7 +1,7 @@
 class_name MissionFactory extends Node
 
 func generate_random(mission_params: MissionParameters) -> MissionData:
-	var mission = MissionData.new()
+	var mission: MissionData = MissionData.new()
 	mission.name = mission_params.possibleNames.pick_random()
 	mission.description = mission_params.possibleDescriptions.pick_random()
 	
--- game/features/props/light_flicker.gd
+++ game/features/props/light_flicker.gd
@@ -4,5 +4,5 @@ extends OmniLight3D
 @export var energy_coefficient: float = 0.05
 @export var energy_floor: float = 0.25
 
-func _process(delta: float) -> void:
+func _process(_delta: float) -> void:
 	light_energy = energy_floor + (noise.get_noise_1d(Time.get_ticks_msec()) * energy_coefficient)
